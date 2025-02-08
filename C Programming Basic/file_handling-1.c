a581c3cb90edccbc90aed725a872e60d712dd434
Abdus Salam <salamshomaji@gmail.com>
2025-02-09 03:01:30 +0600

    Basic file handling of C

---
+++ C Programming Basic/file_handling-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *fptr;
+    fptr = fopen("example.txt","w");
+
+    if(fptr == NULL)
+    {
+        printf("Error opening files\n");
+        return 1;
+    }
+
+    fprintf(fptr, "Hello World!\n");
+    fprintf(fptr, "This is a testing file.n");
+
+    fclose(fptr);
+
+    printf("Data written to file success\n");
+    
+    return 0;
+}

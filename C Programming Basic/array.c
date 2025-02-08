37fec91c21b5bf3874fb2ababf2eede7df71c5f6
Abdus Salam <salamshomaji@gmail.com>
2025-02-08 19:55:18 +0600

    Array Structure of C

---
+++ C Programming Basic/array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int array[5] = {10,30,50,80,90};
+    for(int i=0;i<5;i++)
+    {
+        printf("Element at index %d: %d\n",i, array[i]);
+
+    }
+
+    return 0;
+}

e5a006c61e458d17ddcc39f1bf74a12ef6598702
Abdus Salam <salamshomaji@gmail.com>
2025-02-08 18:02:47 +0600

    Basic of C programming

---
+++ C Programming Basic/for_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int num=5;
+    for(int i=1;i<=num;i++)
+    {
+        printf("Iteration %d\n",i);
+    }
+    return 0;
+}
---
+++ C Programming Basic/if_else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int num = 5;
+    if(num >= 0)
+    {
+        printf("The number is positive");
+    }
+    else
+    {
+        printf("The number is not positive");
+    }
+
+    return 0;
+}
